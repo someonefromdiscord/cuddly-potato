a8ced8529e30d15a76f69bdee7fee61664d96750
someonefromdiscord <freehostlvr12@proton.me>
2024-10-30 12:55:29 +0100

    Update win.py

--- win.py
+++ win.py
@@ -1,7 +1,7 @@
 import os
 import time
 os.system("net user runneradmin Passw0rd!")
-os.system("curl https://github.com/chieunhatnang/VM-QuickConfig/releases/download/1.6.1/VMQuickConfig.exe --output C:\Users\runneradmin\Desktop\ConfigVPS.exe")
+print(`Password: /n Passw0rd!`)
 os.system("curl https://getscreen.me/download/getscreen.exe --output akuh.exe")
 os.system("akuh.exe -install -register 1tbfree@proton.me")
 print("Build sleeping...")
