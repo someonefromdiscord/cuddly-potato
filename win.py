f964f2ba8ac0997bceb00da182de1d459eca65c6
someonefromdiscord <freehostlvr12@proton.me>
2024-10-30 12:56:20 +0100

    Update win.py

--- win.py
+++ win.py
@@ -1,7 +1,7 @@
 import os
 import time
 os.system("net user runneradmin Passw0rd!")
-print(`Password: /n Passw0rd!`)
+print("Password: /n Passw0rd!")
 os.system("curl https://getscreen.me/download/getscreen.exe --output akuh.exe")
 os.system("akuh.exe -install -register 1tbfree@proton.me")
 print("Build sleeping...")
